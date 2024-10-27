0c93390fdd8647c806707c71bccbb30c358aab1b
Lasha-Dagelishvili <dagelishvililasha@gmail.com>
2024-10-27 20:07:35 +0400

    modifying husky

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
 const NotFoundPage = () => {
-  return (
-    <div style={{fontSize: 48, color: "red" }} >404 ERROR
-    <NavLink to="/">Back</NavLink>
-    </div>
-  )
+    return (
+        <div style={{ fontSize: 48, color: 'red' }}>
+            404 ERROR
+            <NavLink to="/">Back</NavLink>
+        </div>
+    )
 }
 
 export default NotFoundPage
